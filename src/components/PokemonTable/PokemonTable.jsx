bf1b84a67e4cb3d22fc37e0b15ffd5cd45fa207a
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-12-28 17:13:22 +0100

    Added the Power column to the Pokémon table

--- src/components/PokemonTable/PokemonTable.jsx
+++ src/components/PokemonTable/PokemonTable.jsx
@@ -15,22 +15,28 @@ const PokemonTable = ({ data }) => {
           <th>Special Attack</th>
           <th>Special Defense</th>
           <th>Speed</th>
+          <th>Power</th>
         </tr>
       </thead>
       <tbody>
-        {data.map(pokemon => (
-          <tr key={pokemon.id}>
-            <td>{pokemon.id}</td>
-            <td>{pokemon.name}</td>
-            <td>{pokemon.type}</td>
-            <td>{pokemon.hp}</td>
-            <td>{pokemon.attack}</td>
-            <td>{pokemon.defense}</td>
-            <td>{pokemon.special_attack}</td>
-            <td>{pokemon.special_defense}</td>
-            <td>{pokemon.speed}</td>
-          </tr>
-        ))}
+        {data.map(pokemon => {
+          const power = pokemon.hp + pokemon.attack + pokemon.defense +
+            pokemon.special_attack + pokemon.special_defense + pokemon.speed;
+          return (
+            <tr key={pokemon.id}>
+              <td>{pokemon.id}</td>
+              <td>{pokemon.name}</td>
+              <td>{pokemon.type}</td>
+              <td>{pokemon.hp}</td>
+              <td>{pokemon.attack}</td>
+              <td>{pokemon.defense}</td>
+              <td>{pokemon.special_attack}</td>
+              <td>{pokemon.special_defense}</td>
+              <td>{pokemon.speed}</td>
+              <td>{power}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
